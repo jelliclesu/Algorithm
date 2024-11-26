3f5e5354ca33aa7adfafec164b5ba6a6cdd5c402
jelliclesu <jelliclesu@naver.com>
2024-11-26 09:51:48 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        int[] arr = new int[10];
+        for (int i = 0; i < numbers.length; i++) {
+            arr[numbers[i]] = numbers[i];
+        }
+        for (int i = 0; i < 10; i++) {
+            if (arr[i] == 0) {
+                answer += i;
+            }
+        }
+        return answer;
+    }
+}
