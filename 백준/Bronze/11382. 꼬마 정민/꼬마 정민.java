187b45e2890d69a75661f2418b0f7b94d62f9e8f
jelliclesu <jelliclesu@naver.com>
2025-02-24 23:59:35 +0900

    [Bronze V] Title: 꼬마 정민, Time: 172 ms, Memory: 17612 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        long a, b, c;
+        a = scanner.nextLong();
+        b = scanner.nextLong();
+        c = scanner.nextLong();
+        System.out.print(a+b+c);
+    }
+}
