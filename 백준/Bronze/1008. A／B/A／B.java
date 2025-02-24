2f631e8bce5e34f67eb25e6be122d0eb8c2bd094
jelliclesu <jelliclesu@naver.com>
2025-02-24 23:07:39 +0900

    [Bronze V] Title: A/B, Time: 176 ms, Memory: 17760 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        double a, b;
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+        System.out.print(a / b);
+    }
+}
