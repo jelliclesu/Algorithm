a4b7bb68167333bd00fceb515213bade0d6d6469
jelliclesu <jelliclesu@naver.com>
2025-02-25 18:53:01 +0900

    [Bronze I] Title: 평균, Time: 208 ms, Memory: 18648 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -5,20 +5,15 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         
         int count = sc.nextInt();
-        double[] scores = new double[count];
-        double maxS = 0;
-        for (int i = 0; i < count; i++) {
-            scores[i] = sc.nextDouble();
-            maxS = Math.max(maxS, scores[i]);
-        }
-        
+        int maxS = 0;
         double sum = 0;
         for (int i = 0; i < count; i++) {
-            scores[i] = scores[i] / maxS * 100;
-            sum += scores[i];
+            int score = sc.nextInt();
+            sum += score;
+            maxS = Math.max(maxS, score);
         }
         
-        System.out.println(sum / count);
+        System.out.println(sum * 100 / maxS / count);
         
     }
 }
