603bec9e5068dd0423b88e2d24d96a947005a186
jelliclesu <jelliclesu@naver.com>
2025-02-25 16:09:47 +0900

    [Bronze III] Title: 오븐 시계, Time: 192 ms, Memory: 18408 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.java
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -8,21 +8,10 @@ public class Main {
         int m = scanner.nextInt();
         int time = scanner.nextInt();
         
-        if (time >= 60) {
-            h += time / 60;
-            m += time % 60;
-        } else {
-            m += time;
-        }
-        
-        if (m >= 60) {
-            h += 1;
-            m -= 60;
-        }
-        
-        if (h > 23) {
-            h -= 24;
-        }
+        m += time;
+        h += m / 60;
+        m %= 60;
+        h %= 24;  
         
         System.out.print(h + " " + m);
     }
