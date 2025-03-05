e82fe5e4b799e7fad24cd69665dbc4e0afcdda3e
jelliclesu <jelliclesu@naver.com>
2025-03-05 16:48:28 +0900

    [level 4] Title: 징검다리, Time: 34.99 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/4/43236. 징검다리/징검다리.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int distance, int[] rocks, int n) {
+        Arrays.sort(rocks);
+        int start = 1;
+        int end = distance;
+        int answer = 0;
+        
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            int remove = 0;
+            int prev = 0;
+            
+            for (int rock : rocks) {
+                long dis = rock - prev;
+                if (dis < mid) {
+                    remove++;
+                    if (remove > n) break;
+                } else {
+                    prev = rock;
+                }
+            }
+            if (distance - prev < mid) {
+                remove++;
+            }
+            
+            if (remove > n) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+                answer = mid;   
+            }
+        }
+        return answer;
+    }
+}
