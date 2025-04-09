7801a1092db0a00a2396bb75d91806e2d910fad4
김지수 <jelliclesu@naver.com>
2025-04-09 11:36:41 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 21.45 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -2,37 +2,35 @@ import java.util.*;
 
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        int answer = 0;
-        Queue<Integer> trucks = new LinkedList<>();
-        Queue<Integer> bridge = new LinkedList<>();
-        int bridgeWeight = 0;
+        Queue<int[]> queue = new LinkedList<>();
+        Queue<Integer> truckQ = new LinkedList<>();
         
         for (int t : truck_weights) {
-            trucks.add(t);
+            truckQ.add(t);
         }
         
-        // 0 을 채워서 다리 길이 지정
-        for (int i = 0; i < bridge_length; i++) {
-            bridge.add(0);
-        }
+        int time = 1;
+        int first = truckQ.poll();
+        queue.add(new int[]{first, time});
+        int sum = first;
         
-        while (!trucks.isEmpty() || bridgeWeight > 0) {
-            answer++;
-            
-            // 다리 큐에서 젤 앞 트럭이 나갈 차례면 제거
-            if (bridge.size() == bridge_length) {
-                bridgeWeight -= bridge.poll();
+        while (!queue.isEmpty()) {
+            time++;
+            int[] arr = queue.peek();
+            if (time - arr[1] == bridge_length) {
+                int[] top = queue.poll();
+                sum -= top[0];
             }
-            
-            // 새로운 트럭이 다리에 올라갈 수 있는지 확인
-            if (!trucks.isEmpty() && bridgeWeight + trucks.peek() <= weight) {
-                int truckWeight = trucks.poll();
-                bridge.add(truckWeight);
-                bridgeWeight += truckWeight;
-            } else {
-                bridge.add(0); // 트럭이 올라올 수 없으면 0 추가 -> 시간 유지
+            if (queue.size() < bridge_length) {
+                if (!truckQ.isEmpty()) {
+                    if (truckQ.peek() + sum <= weight) {
+                        int next = truckQ.poll();
+                        queue.add(new int[]{next, time});
+                        sum += next;
+                    }
+                }
             }
         }
-        return answer;
+        return time;
     }
 }
