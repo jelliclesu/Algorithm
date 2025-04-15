245917a1be39a03563179464cc8e9f2ce9641907
김지수 <jelliclesu@naver.com>
2025-04-15 09:33:33 +0900

    [Bronze II] Title: 소수, Time: 192 ms, Memory: 18352 KB -BaekjoonHub

--- 백준/Bronze/2581. 소수/소수.java
+++ 백준/Bronze/2581. 소수/소수.java
@@ -6,19 +6,12 @@ public class Main {
 
     int m = sc.nextInt();
     int n = sc.nextInt();
-    
+
     int sum = 0;
     int min = 0;
     for (int i = m; i <= n; i++) {
       if (i == 1) continue;
-      boolean isPrime = true;
-      for (int j = 2; j <= Math.sqrt(i); j++) {
-        if (i % j == 0) {
-          isPrime = false;
-          break;
-        }
-      }
-      if (isPrime) {
+      if (isPrime(i)) {
         if (sum == 0) {
           min = i;
         }
@@ -30,5 +23,12 @@ public class Main {
     } else {
       System.out.println(-1);
     }
+  }
+
+  private static boolean isPrime(int i) {
+    for (int j = 2; j <= Math.sqrt(i); j++) {
+      if (i % j == 0) return false;
+    }
+    return true;
   }
 }
