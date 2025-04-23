55de63d843d478d920bcf03dc90ead100ac29b62
김지수 <jelliclesu@naver.com>
2025-04-23 10:15:47 +0900

    [level 2] Title: 주식가격, Time: 60.33 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -2,22 +2,23 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] prices) {
-        int[] answer = new int[prices.length];
+        int n = prices.length;
+        int[] answer = new int[n];
         Stack<Integer> stack = new Stack<>();
         
-        for (int i = 0; i < prices.length; i++) {
-            while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
                 int index = stack.pop();
-                answer[index] = i - index;  // 가격이 떨어진 기간 저장
+                answer[index] = i - index;
             }
-            stack.push(i);  // 현재 인덱스를 스택에 저장
+            stack.push(i);
         }
-
-        // 끝까지 가격이 떨어지지 않은 경우 처리
+        
         while (!stack.isEmpty()) {
             int index = stack.pop();
-            answer[index] = prices.length - index - 1;
+            answer[index] = n - 1 - index;
         }
+        
         return answer;
     }
 }
