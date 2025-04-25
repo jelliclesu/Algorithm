7054d013b590b53833fda453c9d0fbe1593d4b12
김지수 <jelliclesu@naver.com>
2025-04-25 16:55:49 +0900

    [Bronze II] Title: 블랙잭, Time: 104 ms, Memory: 14468 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -17,14 +17,26 @@ public class Main {
     int max = Integer.MIN_VALUE;
     for (int i = n - 1; i >= 2; i--) {
       for (int j = i - 1; j >= 1; j--) {
-        for (int k = j - 1; k >= 0; k--) {
-          int sum = nums[i] + nums[j] + nums[k];
-          if (m >= sum) {
-            max = Math.max(max, sum);
+        
+        int re = m - (nums[i] + nums[j]);
+        int left = 0, right = j - 1;
+        
+        int best = -1;
+        while (left <= right) {
+          int mid = (left + right) / 2;
+          if (nums[mid] <= re) {
+            best = nums[mid];
+            left = mid + 1;
+          } else {
+            right = mid - 1;
           }
+        }
+        
+        if (best != -1) {
+          max = Math.max(max, nums[i] + nums[j] + best);
         }
       }
     }
-      System.out.println(max);
+    System.out.println(max);
   }
 }
