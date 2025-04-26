6b69fddbcc1f3a24f6d74a8b5b10647de304db0a
김지수 <jelliclesu@naver.com>
2025-04-26 14:41:56 +0900

    [Bronze II] Title: 분해합, Time: 192 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int answer = 0;
+        
+        for (int i = 1; i < n; i++) {
+            int sum = i;
+            int temp = i;
+            
+            while (temp != 0) {
+                sum += temp % 10;
+                temp /= 10;
+            }
+            
+            if (sum == n) {
+                answer = i;
+                break;
+            }
+        }
+        
+        System.out.println(answer);
+    }
+}
