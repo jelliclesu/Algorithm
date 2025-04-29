2a75a1ebd8fd9573b42556380d0e1f394c81c7d1
김지수 <jelliclesu@naver.com>
2025-04-29 09:59:24 +0900

    [level 2] Title: 가장 큰 수, Time: 193.19 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,15 +2,13 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        String[] arr = Arrays.stream(numbers)
-                             .mapToObj(String::valueOf)
-                             .toArray(String[]::new);
-
-        // 정렬 기준 설정 (a + b vs b + a)
-        Arrays.sort(arr, (a, b) -> (b + a).compareTo(a + b));
-
-        if (arr[0].equals("0")) return "0";
-
-        return String.join("", arr);
+        
+        String[] strNums = Arrays.stream(numbers).mapToObj(String::valueOf).toArray(String[]::new);
+        
+        Arrays.sort(strNums, (a, b) -> (b + a).compareTo(a + b));
+        
+        if (strNums[0].equals("0")) return "0";
+        
+        return String.join("", strNums);
     }
 }
