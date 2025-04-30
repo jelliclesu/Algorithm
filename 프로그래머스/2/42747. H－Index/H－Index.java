98f69df56a1ca695817116470f8fbe918e84aa02
김지수 <jelliclesu@naver.com>
2025-04-30 10:25:19 +0900

    [level 2] Title: H-Index, Time: 0.94 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -2,19 +2,17 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] citations) {
-        int answer = 0;
-        int n = citations.length;
-        
         Arrays.sort(citations);
+        int len = citations.length;
+        int answer = 0;
         
-        for (int i = 0; i < n; i++) {
-            int h = n - i; // 현재 논문 이상 인용된 논문의 개수
+        for (int i = 0; i < len; i++) {
+            int h = len - i;
             if (citations[i] >= h) {
                 answer = h;
-                break;
+                return answer;
             }
         }
-        
         return answer;
     }
 }
