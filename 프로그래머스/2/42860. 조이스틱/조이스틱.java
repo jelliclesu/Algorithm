698cb598832bc2363e83bfc61730ac971b96e23c
김지수 <jelliclesu@naver.com>
2025-05-02 11:27:43 +0900

    [level 2] Title: 조이스틱, Time: 0.06 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -1,25 +1,25 @@
 class Solution {
     public int solution(String name) {
-        int answer = 0;
         int len = name.length();
-        
         int move = len - 1;
-
+        int count = 0;
+        
         for (int i = 0; i < len; i++) {
-            char c = name.charAt(i);
-            answer += Math.min(c - 'A', 'Z' - c + 1);
+            char ch = name.charAt(i);
+            // 위아래
+            count += Math.min(ch - 'A', 'Z' - ch + 1);
 
-            // 다음 위치부터 연속된 A 가 끝나는 지점
+            // 다음 위치부터 연속된 'A'의 개수
             int next = i + 1;
             while (next < len && name.charAt(next) == 'A') {
                 next++;
             }
 
-            // 좌우이동 최소 횟수 구하기 (순서대로 가기 vs 뒤로 돌아가기)
-            move = Math.min(move, (i * 2) + len - next);
-            move = Math.min(move, (len - next) * 2 + i);
+            // 왼오 최소값 구하기
+            move = Math.min(move, (i * 2) + len - next);    // 오른쪽으로 가다가 우회
+            move = Math.min(move, (len - next) * 2 + i);    // 왼쪽으로 가다가 우회
         }
 
-        return answer + move;
+        return count + move;
     }
 }
