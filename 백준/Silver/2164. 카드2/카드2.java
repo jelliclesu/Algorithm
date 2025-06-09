93786899e7b23babacd98bbb267b43107f25d658
김지수 <jelliclesu@naver.com>
2025-06-09 09:58:08 +0900

    [Silver IV] Title: 카드2, Time: 228 ms, Memory: 48960 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -3,17 +3,18 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
         int n = sc.nextInt();
+
         Queue<Integer> queue = new LinkedList<>();
-        
         for (int i = 1; i <= n; i++) {
             queue.add(i);
         }
-        while (queue.size() > 1) {
+
+        while (queue.size() != 1) {
             queue.poll();
             queue.add(queue.poll());
         }
-        System.out.print(queue.poll());
+
+        System.out.println(queue.poll());
     }
 }
