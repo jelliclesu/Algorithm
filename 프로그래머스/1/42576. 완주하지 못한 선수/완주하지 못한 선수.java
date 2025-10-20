a409646e912b3efb4a7b64389038318645e36f44
김지수 <jelliclesu@naver.com>
2025-10-20 20:50:39 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 275.59 ms, Memory: 97.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -5,11 +5,11 @@ class Solution {
         Arrays.sort(participant);
         Arrays.sort(completion);
         
-        for (int i = 0; i < completion.length; i++) {
-            if (!participant[i].equals(completion[i])) {
-                return participant[i];
-            }
+        int n = completion.length;
+
+        for (int i = 0; i < n; i++) {
+            if (!participant[i].equals(completion[i])) return participant[i];
         }
-        return participant[participant.length - 1];
+        return participant[n];
     }
 }
