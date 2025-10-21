37645685277a517b816d2b6e19ed14b076cc2a83
김지수 <jelliclesu@naver.com>
2025-10-21 18:12:54 +0900

    [level 1] Title: 폰켓몬, Time: 5.06 ms, Memory: 88.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,17 +2,13 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        int answer = 0;
+        int n = nums.length / 2;
         Set<Integer> set = new HashSet<>();
-        for (int i = 0 ; i < nums.length; i++) {
-            if (!set.contains(nums[i])) {
-                set.add(nums[i]);
-                answer++;
-            }
-            if (set.size() == nums.length / 2) {
-                return answer;
-            }
+        
+        for (int num : nums) {
+            set.add(num);
+            if (set.size() == n) return n;
         }
-        return answer;
+        return set.size();
     }
 }
