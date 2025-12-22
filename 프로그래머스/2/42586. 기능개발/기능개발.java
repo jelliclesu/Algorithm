d31dc90f67c86d534a430c1fa59a625ac8841081
김지수 <jelliclesu@naver.com>
2025-12-22 19:59:49 +0900

    [level 2] Title: 기능개발, Time: 3.23 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -3,26 +3,21 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         List<Integer> answer = new ArrayList<>();
-        Queue<Integer> queue = new LinkedList<>();
         
-        for (int i = 0; i < progresses.length; i++) {
-            int r = 100 - progresses[i];
-            if (r % speeds[i] != 0) {
-                queue.add(r / speeds[i] + 1);
-            } else {
-                queue.add(r / speeds[i]);
-            }
-        }
-        while (!queue.isEmpty()) {
-            int top = queue.poll();
-            int count = 1;
-            while (!queue.isEmpty() && top >= queue.peek()) {
-                queue.poll();
+        int count = 1;
+        int x = (int) Math.ceil((100 - progresses[0]) / (double) speeds[0]);
+
+        for (int i = 1; i < progresses.length; i++) {
+            if (progresses[i] + speeds[i] * x >= 100) {
                 count++;
+                continue;
             }
             answer.add(count);
+            count = 1;
+            x = (int) Math.ceil((100 - progresses[i]) / (double) speeds[i]);
+            
         }
-        
+        answer.add(count);
         return answer.stream().mapToInt(i -> i).toArray();
     }
 }
