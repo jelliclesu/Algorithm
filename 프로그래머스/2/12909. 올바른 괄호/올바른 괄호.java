51a3828de22c3beb71db436e0f717401c863e5eb
김지수 <jelliclesu@naver.com>
2025-12-24 18:51:59 +0900

    [level 2] Title: 올바른 괄호, Time: 21.43 ms, Memory: 54.7 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -12,7 +12,7 @@ class Solution {
                     stack.pop();
                 }
             } else {
-                stack.push(')');
+                stack.push(c);
             }
         }
 
