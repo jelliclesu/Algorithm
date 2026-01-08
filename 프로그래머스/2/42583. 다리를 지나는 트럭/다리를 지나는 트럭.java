a5ac2e1dbd65eb0168af2ae12cd02ef86518def4
김지수 <jelliclesu@naver.com>
2026-01-08 11:41:02 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 33.69 ms, Memory: 87.4 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,36 +1,38 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        Queue<int[]> queue = new LinkedList<>();
-        Queue<Integer> truckQ = new LinkedList<>();
+        LinkedList<Integer> trucks = new LinkedList<>();
+        LinkedList<Integer> bridge = new LinkedList<>();
         
-        for (int t : truck_weights) {
-            truckQ.add(t);
+        trucks = Arrays.stream(truck_weights).boxed().collect(Collectors.toCollection(LinkedList::new));
+        
+        for (int i = 0; i < bridge_length - 1; i++) {
+            bridge.add(0);
         }
         
+        int top = trucks.poll();
+        int total_weight = top;
         int time = 1;
-        int first = truckQ.poll();
-        queue.add(new int[]{first, time});
-        int sum = first;
         
-        while (!queue.isEmpty()) {
+        bridge.add(top);
+        
+        while (!trucks.isEmpty()) {
+            int next = trucks.peek();
+            int out = bridge.poll();
+            total_weight -= out;
+            
+            if (total_weight + next > weight) {
+                bridge.add(0);
+            } else {
+                bridge.add(trucks.poll());
+                total_weight += next;
+            }
             time++;
-            int[] arr = queue.peek();
-            if (time - arr[1] == bridge_length) {
-                int[] top = queue.poll();
-                sum -= top[0];
-            }
-            if (queue.size() < bridge_length) {
-                if (!truckQ.isEmpty()) {
-                    if (truckQ.peek() + sum <= weight) {
-                        int next = truckQ.poll();
-                        queue.add(new int[]{next, time});
-                        sum += next;
-                    }
-                }
-            }
         }
+        time += bridge_length;        
+        
         return time;
     }
 }
