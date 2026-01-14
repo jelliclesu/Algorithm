84a27f586dd74e685c26ce1805ab17052f48a176
김지수 <jelliclesu@naver.com>
2026-01-14 15:49:04 +0900

    [level 2] Title: 의상, Time: 0.19 ms, Memory: 90.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,19 +2,18 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        Map<String, Integer> map = new HashMap<>();
-
-        for (String[] c : clothes) {
-            String type = c[1];
-            map.put(type, map.getOrDefault(type, 0) + 1);
-        }
-
         int answer = 1;
-
-        for (int count : map.values()) {
-            answer *= (count + 1);
+        HashMap<String, ArrayList<String>> map = new HashMap<>();
+        
+        for (String[] arr : clothes) {
+            map.putIfAbsent(arr[1], new ArrayList<>());
+            map.get(arr[1]).add(arr[0]);
         }
-
+        
+        for (String key : map.keySet()){
+            answer *= map.get(key).size() + 1;
+        }
+        
         return answer - 1;
     }
 }
